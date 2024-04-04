6282c6202482e1f8e1776d0fe829ab3e6a002b7b
lanchu <hoanglan.ict@gmail.com>
2024-04-04 23:58:31 +0700

    temp

--- sliding_window/sliding_window_maximum.py
+++ sliding_window/sliding_window_maximum.py
@@ -1,31 +1,27 @@
 # https://leetcode.com/problems/sliding-window-maximum/
 
-import heapq
-
+from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
         start, end = 0, k - 1
-
-        window = nums[start : end + 1]
-        heapq._heapify_max(window)
-
         result = []
+        stack = deque()
+        for i in range(end + 1):
+            while stack and stack[0] < 
         while True:
             print(window)
-            max_item = window[0]
-            result.append(max_item)
+            max_window = heapq.heappop(window)
+            result.append(max_window * -1)
+
+            if max_window == nums[start]:
+                heapq.heappush(window, max_window)
             start += 1
+
             end += 1
             if end >= len(nums):
                 break
-            if nums[start] == window[0]:
-                print("remove")
-                heapq._heappop_max(window)
+
             heapq.heappush(window, nums[end])
-            print(window)
 
         return result
-
-
-print(Solution().maxSlidingWindow(nums=[1, 3, -1, -3, 5, 3, 6, 7], k=3))
