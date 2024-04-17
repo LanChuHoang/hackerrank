c3ea1b12ee35e7a3d6f288a8ac1db95add7e1a28
lan.chu <lan.chu@datx.com.vn>
2024-04-17 17:30:20 +0700

    Refactor n_queens.py to improve readability and maintainability

--- backtracking/n_queens.py
+++ backtracking/n_queens.py
@@ -8,28 +8,35 @@ class Solution:
         board = [["." for _ in range(n)] for _ in range(n)]
 
         column_placed = [False for _ in range(n)]
-        diag_placed = [False for _ in range((n - 1) * 2 + 1)]
+        diag1_placed = [False for _ in range((n - 1) * 2 + 1)]
+        diag2_placed = diag1_placed.copy()
 
         def dfs(i: int):
-            nonlocal result, board, column_placed, diag_placed
+            nonlocal result, board, column_placed, diag1_placed
             if i == n:
                 serialized_board = ["".join(row) for row in board]
                 result.append(serialized_board)
                 return
 
             for j in range(n):
-                if not column_placed[j] and not diag_placed[i - j]:
+                if (
+                    not column_placed[j]
+                    and not diag1_placed[i - j]
+                    and not diag2_placed[n - 1 - i - j]
+                ):
                     board[i][j] = "Q"
                     column_placed[j] = True
-                    diag_placed[i - j] = True
+                    diag1_placed[i - j] = True
+                    diag2_placed[n - 1 - i - j] = True
                     dfs(i + 1)
                     board[i][j] = "."
                     column_placed[j] = False
-                    diag_placed[i - j] = False
+                    diag1_placed[i - j] = False
+                    diag2_placed[n - 1 - i - j] = False
 
         dfs(0)
 
         return result
 
 
-print(Solution().solveNQueens(4))
+# print(Solution().solveNQueens(4))
