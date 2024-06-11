b5d3d07795807731ad982a49221cd1a731c4db33
lan.chu <lan.chu@datx.com.vn>
2024-06-11 09:26:28 +0700

    feat: Optimize longestCommonSubsequence_v1 function using memoization

--- dp/longest_common_subsequence.py
+++ dp/longest_common_subsequence.py
@@ -2,6 +2,30 @@
 
 
 class Solution:
+    def longestCommonSubsequence_v1(self, text1: str, text2: str) -> int:
+        # For every i, j in text1 and text2
+        # lcs(i, j) = lcs(i + 1, j + 1) + 1 if text1[i] == text2[j]
+        # else = max(lcs(i + 1, j), lcs(i, j + 1))
+
+        memo = dict()
+
+        def lcs(i: int, j: int):
+            if i >= len(text1) or j >= len(text2):
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            memo[(i, j)] = (
+                1 + lcs(i + 1, j + 1)
+                if text1[i] == text2[j]
+                else max(lcs(i + 1, j), lcs(i, j + 1))
+            )
+
+            return memo[(i, j)]
+
+        return lcs(0, 0)
+
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp_table = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
         for i in reversed(range(len(text1))):
