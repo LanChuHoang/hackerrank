1fa946cfbe7955b22a147749b9223677e2f79bc8
lanchu <hoanglan.ict@gmail.com>
2024-07-17 21:31:06 +0700

    Refactor Solution class in minimum_window_substring.py for improved readability and performance

--- sliding_window/minimum_window_substring.py
+++ sliding_window/minimum_window_substring.py
@@ -1,68 +1,91 @@
-# https://leetcode.com/problems/minimum-window-substring/description/
-
-
 class Solution:
+    def minWindow_v1(self, s: str, t: str) -> str:
+        # Idea: maintain a sliding window in s
+        # try to extend the right boundary until the current window is a valid solution
+        # then reduce the left boundary until it breaks the valid condition
+        # during the reduce phase -> record the shortest window
+        # Idea: How to check the valid condition
+        # Simply storing 2 dicts, and maintain 2 variables have and need
+        # every time we meet some char that break the valid condition -> update have
+        s_count = dict()
+        t_count = dict()
+        for c in t:
+            t_count[c] = t_count.get(c, 0) + 1
 
-    def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+        l, r, n = 0, 0, len(s)
+        res, res_l, res_r = n + 1, 0, 0
+        have, need = 0, len(t_count)
+        if s[r] in t_count:
+            s_count[s[r]] = 1
+            if s_count[s[r]] == t_count[s[r]]:
+                have = 1
+
+        while l < n and r < n:
+            if have == need:
+                wlen = r - l + 1
+                if wlen < res:
+                    res, res_l, res_r = wlen, l, r
+
+                if s[l] in t_count:
+                    s_count[s[l]] -= 1
+                    if s_count[s[l]] == t_count[s[l]] - 1:
+                        have -= 1
+                l += 1
+            else:
+                r += 1
+                if r < n and s[r] in t_count:
+                    s_count[s[r]] = s_count.get(s[r], 0) + 1
+                    if s_count[s[r]] == t_count[s[r]]:
+                        have += 1
+        return s[res_l : res_r + 1] if res <= n else ""
+
+    def minWindow(self, s: str, t: str):
+        # Idea: optimize validation check by using a single map as a array
+        # and a single variable need
+        # In the expand phase:
+        # - if the count of the current char c > 0
+        # (means c in t and the current window doesnt have enough number of c)
+        # -> decrease need
+        # - Then decrease the count of the current char (dont care whether c in t or not,
+        # because if c in t -> the count will be 0, ow negative -> so in the reduce phase
+        # we can use it as a flag of what in t or not, for update the "need")
+        # In the reduce phase:
+        # - while need == 0:
+        #   - update the solution
+        #   - if we meet some count that == 0, that mean if we shrink the left, the number of that char
+        # in the window < the required from t
+        # -> so we need to increase the "need"
+        #   - char that not in t does not effect, because count never > 0 (at most 0) so it never meets the
+        # above check
+        if not s or not t or len(s) < len(t):
             return ""
 
-        count_t, count_window = dict(), dict()
-
+        visit_times = [0] * (ord("z") + 1)
         for c in t:
-            count_t[c] = count_t.get(c, 0) + 1
+            visit_times[ord(c)] += 1
 
-        have, need = len(count_window), len(count_t)
-        start = 0
-        min_length, res_start, res_end = None, None, None
+        l, r, n = 0, 0, len(s)
+        res, res_l = n + 1, 0
+        need = len(t)
+        while r < n:
+            if visit_times[ord(s[r])] > 0:
+                need -= 1
+            visit_times[ord(s[r])] -= 1
+            r += 1
 
-        for end in range(len(s)):
-            if s[end] not in count_t:
-                continue
+            while need == 0:
+                wlen = r - l
+                if wlen < res:
+                    res, res_l = wlen, l
 
-            count_window[s[end]] = count_window.get(s[end], 0) + 1
-
-            if count_window[s[end]] == count_t[s[end]]:
-                have += 1
-                # print("increase have for", s[end], count_window, have, count_t, need)
-
-            # Try to minimize the solution
-            while have == need:
-                # print(
-                #     "found",
-                #     start,
-                #     end,
-                #     "-",
-                #     s[start : end + 1],
-                #     count_window,
-                #     have,
-                #     count_t,
-                #     need,
-                # )
-                cur_length = end - start + 1
-                if min_length is None or cur_length < min_length:
-                    min_length = cur_length
-                    res_start = start
-                    res_end = end
-
-                # Pop the first element
-                if s[start] in count_t:
-                    count_window[s[start]] -= 1
-                    if count_window[s[start]] == count_t[s[start]] - 1:
-                        have -= 1
-                        # print(
-                        #     "decrease have for",
-                        #     s[end],
-                        #     count_window,
-                        #     have,
-                        #     count_t,
-                        #     need,
-                        # )
-
-                start += 1
-
-        return s[res_start : res_end + 1] if res_start is not None else ""
+                if visit_times[ord(s[l])] == 0:
+                    need += 1
+                visit_times[ord(s[l])] += 1
+                l += 1
+        return s[res_l : res_l + res] if res <= n else ""
 
 
-# print(Solution().minWindow(s="cabwefgewcwaefgcf", t="cae"))
-# print(Solution().minWindow(s="acbbaca", t="aba"))
+# s = Solution()
+# print(s.minWindow(s="ADOBECODEBANC", t="ABC"))
+# print(s.minWindow(s="a", t="a"))
+# print(s.minWindow(s="a", t="aa"))
